52003645a8822f5e4fa683fbfb618b4ff769d312
Patrick Burns <burns212@gmail.com>
2022-11-30 20:34:26 -0600

    initial tutorial done chapt 6 but not practice

--- levelup/urls.py
+++ levelup/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 from levelupapi.views import register_user, check_user
+from django.conf.urls import include
+from rest_framework import routers
+from levelupapi.views import GameTypeView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'gametypes', GameTypeView, 'gametype')
 
 urlpatterns = [
     path('register', register_user),
     path('checkuser', check_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
--- levelupapi/views/game_type.py
+++ levelupapi/views/game_type.py
@@ -15,6 +15,9 @@ class GameTypeView(ViewSet):
         Returns:
             Response -- JSON serialized game type
         """
+        game_type = GameType.objects.get(pk=pk)
+        serializer = GameTypeSerializer(game_type)
+        return Response(serializer.data)
 
 
     def list(self, request):
@@ -23,3 +26,13 @@ class GameTypeView(ViewSet):
         Returns:
             Response -- JSON serialized list of game types
         """
+        game_types = GameType.objects.all()
+        serializer = GameTypeSerializer(game_types, many=True)
+        return Response(serializer.data)
+
+class GameTypeSerializer(serializers.ModelSerializer):
+    """JSON serializer for game types
+    """
+    class Meta:
+        model = GameType
+        fields = ('id', 'label')
